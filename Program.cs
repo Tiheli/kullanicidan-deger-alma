f97e3c9f64f36b8239b631a25b739ce032616ba0
Tiheli <toklusuleyman24@gmail.com>
2022-10-22 15:43:55 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kullanicidan_deger_alma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string isim;
+            Console.WriteLine(" isim giriniz.. ");
+            isim= Console.ReadLine();
+            Console.WriteLine("merhaba, {" + isim + "}");
+            Console.ReadKey();
+        }
+
+    }
+}
